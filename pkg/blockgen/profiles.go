b60acb1719081a013b1caf92a49aa4429b46e87e
Nathaniel Graham <ngraham@redhat.com>
2023-02-10 20:21:09 +0000

    cpudata

--- pkg/blockgen/profiles.go
+++ pkg/blockgen/profiles.go
@@ -133,6 +133,16 @@ var (
 			2 * time.Hour,
 			// 10,000 series per block.
 		}, 10000, 1),
+		"cpubench": cpudata([]time.Duration{
+			// One week, from newest to oldest, in the same way Thanos compactor would do.
+			2 * time.Hour,
+			2 * time.Hour,
+			2 * time.Hour,
+			8 * time.Hour,
+			8 * time.Hour,
+			16 * time.Hour,
+			// 10,000 series per block.
+		}, 10000, 1),
 	}
 )
 
@@ -210,6 +220,66 @@ func realisticK8s(ranges []time.Duration, rolloutInterval time.Duration, apps in
 
 				lastRollout -= durToMilis(rolloutInterval)
 			}
+
+			if err := blockEncoder(b); err != nil {
+				return err
+			}
+			maxt = mint
+		}
+		return nil
+	}
+}
+
+func cpudata(ranges []time.Duration, apps int, metricsPerApp int) PlanFn {
+	return func(ctx context.Context, maxTime model.TimeOrDurationValue, extLset labels.Labels, blockEncoder func(BlockSpec) error) error {
+
+		// Align timestamps as Prometheus would do.
+		maxt := rangeForTimestamp(maxTime.PrometheusTimestamp(), durToMilis(2*time.Hour))
+
+		// All our series are gauges.
+		common := SeriesSpec{
+			Targets: apps,
+			Type:    Gauge,
+			Characteristics: seriesgen.Characteristics{
+				Max:            200000000,
+				Min:            10000000,
+				Jitter:         30000000,
+				ScrapeInterval: 15 * time.Second,
+				ChangeInterval: 1 * time.Hour,
+			},
+		}
+
+		for _, r := range ranges {
+			mint := maxt - durToMilis(r) + 1
+
+			if ctx.Err() != nil {
+				return ctx.Err()
+			}
+
+			b := BlockSpec{
+				Meta: metadata.Meta{
+					BlockMeta: tsdb.BlockMeta{
+						MaxTime:    maxt,
+						MinTime:    mint,
+						Compaction: tsdb.BlockMetaCompaction{Level: 1},
+						Version:    1,
+					},
+					Thanos: metadata.Thanos{
+						Labels:     extLset.Map(),
+						Downsample: metadata.ThanosDownsample{Resolution: 0},
+						Source:     "blockgen",
+					},
+				},
+			}
+
+			s := common
+
+			s.Labels = labels.Labels{
+				{Name: "__name__", Value: fmt.Sprintf("node_namespace_pod_container:container_cpu_usage_seconds_total:sum_rate")},
+			}
+			s.MinTime = mint
+			s.MaxTime = maxt
+			b.Series = append(b.Series, s)
 
 			if err := blockEncoder(b); err != nil {
 				return err
